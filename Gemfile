6963907b09891db33250cb08be53951a0f34e9f9
Steven-Courrot <steven.code@proton.me>
2022-10-24 18:41:01 +0200

    third commit

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@ source "https://rubygems.org"
 ruby '2.7.4'
 gem 'sinatra'
 gem 'puma'
+gem 'rack'
 gem 'shotgun'
+gem 'pry'
+gem 'rubocop'
+gem 'rspec'
---
+++ lib/controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < Sinatra::Base
+  
+  get '/' do
+    erb :index 
+  end
+  
+  get '/gossips/new/' do
+    puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+  end
+  
+  post '/gossips/new/' do
+    Gossip.new(les_entrées_du_gossip).save
+  end
+
+end
